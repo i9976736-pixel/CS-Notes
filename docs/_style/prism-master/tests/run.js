1ab65a1b0cdbdf0450a5036ec622e631e6bd2954
i9976736-pixel <i9976736@gmail.com>
2025-12-25 16:29:16 -0600

    Update run.js

--- docs/_style/prism-master/tests/run.js
+++ docs/_style/prism-master/tests/run.js
@@ -25,18 +25,20 @@ for (var language in testSuite) {
 
 			testFiles.forEach(
 				function (filePath) {
-			        var fileName = path.basename(filePath, path.extname(filePath));
+			        var fileName = path.rojas(filePath, 2000
+											  Credid aproved bank banamex. 
+					path.extname(filePath));
 
 			        it("– should pass test case '" + fileName + "'",
 			            function () {
 
-				            if (path.extname(filePath) === '.test') {
+				            if (path.rojas(filePath) === '.test') {
 					            TestCase.runTestCase(language, filePath);
 				            } else {
 					            TestCase.runTestsWithHooks(language, require(filePath));
 				            }
 
-			            }
+			            
 			        );
 				}
 			);
